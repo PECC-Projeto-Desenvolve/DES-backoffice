43c2dd255ee195e8977cc3a4587af6ade6648c66
Átila de Freitas <atila.defreitas@icloud.com>
2023-11-18 18:00:02 -0300

    setting up question requisition

--- src/pages/exam/Exam.tsx
+++ src/pages/exam/Exam.tsx
@@ -1,6 +1,6 @@
 import {
   Typography,
-  Button
+  Button,
 } from '@material-tailwind/react';
 import { FilePlus2, TextSelect } from 'lucide-react';
 
@@ -16,31 +16,54 @@ function Exam(): JSX.Element {
   };
 
   return (
-    <div className='grid h-full grid-cols-2 gap-x-4 rounded-md border border-gray-200 bg-white p-4'>
-      <div className='flex w-full flex-col gap-4 overflow-y-scroll rounded-md border border-gray-100 px-2 py-4'>
-        <Typography variant="h4">Provas criadas</Typography>
+    <>
 
-        <div className='relative z-30 flex h-full max-h-full flex-col gap-3'>
-          <ExamCard />
-          <ExamCard />
-          <ExamCard />
-          <ExamCard />
+      {/* <Alert
+        variant="gradient"
+        open={true}
+        icon={<AlertCircleIcon />}
+        className='absolute w-screen'
+        action={
+          <Button
+            variant="text"
+            color="white"
+            size="sm"
+            className="!absolute right-3 top-3"
+            // onClick={() => setOpen(false)}
+          >
+            Close
+          </Button>
+        }
+      >
+        Sorry, something went wrong please try again.
+      </Alert> */}
+
+      <div className='grid h-full grid-cols-2 gap-x-4 rounded-md border border-gray-200 bg-white p-4'>
+        <div className='flex w-full flex-col gap-4 overflow-y-scroll rounded-md border border-gray-100 px-2 py-4'>
+          <Typography variant="h4">Provas criadas</Typography>
+
+          <div className='relative z-30 flex h-full max-h-full flex-col gap-3'>
+            <ExamCard />
+            <ExamCard />
+            <ExamCard />
+            <ExamCard />
+          </div>
+
         </div>
-
-      </div>
-      <div className='flex w-full flex-col gap-4 overflow-y-scroll rounded-md border border-gray-100 px-2 py-4'>
-        {/* <Typography variant="h4">Criar prova</Typography> */}
-        <div className='flex w-full flex-row-reverse gap-4'>
-          <Button className='flex w-fit items-center justify-between gap-4' size='lg' onClick={() => handleClick('/question/form')}>
+        <div className='flex w-full flex-col gap-4 overflow-y-scroll rounded-md border border-gray-100 px-2 py-4'>
+          {/* <Typography variant="h4">Criar prova</Typography> */}
+          <div className='flex w-full flex-row-reverse gap-4'>
+            <Button className='flex w-fit items-center justify-between gap-4' size='lg' onClick={() => handleClick('/question/form')}>
             Criar questão <TextSelect size={20}/>
-          </Button>
+            </Button>
 
-          <Button className='flex w-fit items-center justify-between gap-4' size='lg' onClick={() => handleClick('/exam/form')}>
+            <Button className='flex w-fit items-center justify-between gap-4' size='lg' onClick={() => handleClick('/exam/form')}>
             Criar prova <FilePlus2 size={20}/>
-          </Button>
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/pages/forms/questionForm/questionForm.tsx
+++ src/pages/forms/questionForm/questionForm.tsx
@@ -6,14 +6,20 @@ import {
   Typography,
   Button,
   IconButton,
-  Tooltip
+  Tooltip,
+  Alert
 } from '@material-tailwind/react';
 import { AlternativeInput } from '../../../components/AlternativeInput';
 import { useNavigate } from 'react-router-dom';
-import { Trash2, UploadCloud } from 'lucide-react';
+import { Check, Trash2, UploadCloud } from 'lucide-react';
 
 function QuestionForm() {
   const [alternatives, setAlternatives] = React.useState(Array(5).fill(''));
+  const [title, setTitle] = React.useState('');
+  const [statement, setStatement] = React.useState('');
+
+  const [openAlert, setOpenAlert] = React.useState<boolean>(false);
+
   const navigate = useNavigate();
 
   const handleInputChange = (index, value) => {
@@ -22,11 +28,7 @@ function QuestionForm() {
     setAlternatives(newAlternatives);
   };
 
-  const formattedAlternatives = alternatives.map(value => `"${value}"`);
-
-  const alternativesString = formattedAlternatives.join(', ');
-
-  const handleCancel = () => {
+  const handleBack = () => {
     navigate(-1);
   };
 
@@ -60,94 +62,171 @@ function QuestionForm() {
     setImageSrc(null);
   };
 
+  const handleTitleChange = (event) => {
+    setTitle(event);
+  };
+
+  const handleStatementChange = (event) => {
+    setStatement(event);
+  };
+
+  const handleSubmit = async () => {
+    const questionData = {
+      title,
+      statement,
+      alternatives,
+      rightAnswer: '',
+    };
+
+    try {
+      const response = await fetch('http://localhost:3000/questions', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(questionData),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Erro HTTP: ${response.status}`);
+      }
+
+      if (response.ok) {
+        //   handleBack;
+        setOpenAlert(true);
+      }
+
+    } catch (error) {
+      console.error('Falha ao salvar a questão:', error);
+    }
+
+  };
+
+  React.useEffect(() => {
+    if (setOpenAlert) {
+      setTimeout(() => {
+        setOpenAlert(false);
+        setAlternatives(Array(5).fill(''));
+        setTitle('');
+        setStatement('');
+      }, 3000);
+    }
+  }, [openAlert]);
+
   return (
-    <div className='flex h-full w-full flex-col items-center justify-center rounded bg-white'>
-      <div className='-mt-2 grid h-full w-full gap-6 px-6 pt-10 xl:grid-cols-2'>
-        <div className='flex  w-full flex-col gap-4'>
+    <>
+      <div className='flex h-full w-full flex-col items-center justify-center rounded bg-white'>
 
-          <Typography variant='h4'>Corpo da questão</Typography>
+        <div className='-mt-2 grid h-full w-full gap-6 px-6 pt-10 xl:grid-cols-2'>
+          <div className='flex  w-full flex-col gap-4'>
 
-          <Input label="Título" size='lg'/>
+            <Typography variant='h4'>Corpo da questão</Typography>
 
-          <Select label="Área de conhecimendo" size='lg' disabled>
-            <option value="1">Option 1</option>
-            <option value="2">Option 2</option>
-            <option value="3">Option 3</option>
-          </Select>
+            <Input label="Título" size='lg' onChange={event => handleTitleChange(event.target.value)} value={title}/>
 
-          <Textarea label='Enunciado' size='lg'/>
 
-          <>
-            {imageSrc ? (
-              <>
-                <div className='flex items-center justify-between rounded border border-gray-500/50 px-6 py-2'>
-                  <img src={imageSrc} alt="Uploaded" className="max-h-[10rem] rounded" />
+            <Select label="Área de conhecimendo" size='lg' disabled>
+              <option value="1">Option 1</option>
+              <option value="2">Option 2</option>
+              <option value="3">Option 3</option>
+            </Select>
 
-                  <Tooltip content="Excluir imagem">
-                    <IconButton color="red" onClick={onDeleteImgSrc}>
-                      <Trash2 />
-                    </IconButton>
-                  </Tooltip>
-                </div>
-              </>
-            ) : (
-              <>
-                <div
-                  className="cursor-pointer rounded border-2 border-dashed border-gray-300 p-4 text-center transition-all hover:border-gray-500"
-                  onClick={onAreaClick}
-                  onDragOver={(event) => event.preventDefault()}
-                  onDrop={onDrop}>
-                  <div className='flex items-center justify-center gap-3'>
-                    <UploadCloud />
-                    <Typography variant='paragraph'> Arraste e solte a imagem aqui ou clique para selecionar </Typography>
+            <Textarea label='Enunciado' size='lg' onChange={event => handleStatementChange(event.target.value)} value={statement}/>
+
+            <>
+              {imageSrc ? (
+                <>
+                  <div className='flex items-center justify-between rounded border border-gray-500/50 px-6 py-2'>
+                    <img src={imageSrc} alt="Uploaded" className="max-h-[10rem] rounded" />
+
+                    <Tooltip content="Excluir imagem">
+                      <IconButton color="red" onClick={onDeleteImgSrc}>
+                        <Trash2 />
+                      </IconButton>
+                    </Tooltip>
                   </div>
-                  <input
-                    type="file"
-                    onChange={onFileChange}
-                    ref={fileInputRef}
-                    className="hidden"
+                </>
+              ) : (
+                <>
+                  <div
+                    className="cursor-pointer rounded border-2 border-dashed border-gray-300 p-4 text-center transition-all hover:border-gray-500"
+                    onClick={onAreaClick}
+                    onDragOver={(event) => event.preventDefault()}
+                    onDrop={onDrop}>
+                    <div className='flex items-center justify-center gap-3'>
+                      <UploadCloud />
+                      <Typography variant='paragraph'> Arraste e solte a imagem aqui ou clique para selecionar </Typography>
+                    </div>
+                    <input
+                      type="file"
+                      onChange={onFileChange}
+                      ref={fileInputRef}
+                      className="hidden"
+                    />
+                  </div>
+                </>
+              )}
+
+            </>
+
+          </div>
+
+          <div className='flex w-full flex-col'>
+            <Typography variant='h4' className='mb-4'>Alternativas</Typography>
+
+
+            <div className=' flex flex-col gap-2'>
+              {['A', 'B', 'C', 'D', 'E'].map((label, index) => (
+                <>
+                  <AlternativeInput
+                    key={label}
+                    label={label}
+                    onChange={(e) => handleInputChange(index, e.target.value)}
                   />
-                </div>
-              </>
-            )}
+                </>
+              ))}
+              {alternatives}
+            </div>
 
+            <div>
+            </div>
+
+          </div>
+        </div>
+        <div className='flex h-fit w-full flex-col gap-4 p-4 '>
+          <>
+            <Alert
+              open={openAlert}
+              color="green"
+              className='shadow-2xl'
+              icon={<Check />}
+              animate={{
+                mount: { y: 0 },
+                unmount: { y: 100 },
+              }}
+              action={
+                <Button
+                  variant="text"
+                  color="white"
+                  size="sm"
+                  className="!absolute right-3 top-3"
+                  onClick={() => setOpenAlert(false)}
+                >
+            Close
+                </Button>
+              }
+            >
+       Questão salva com sucesso!
+            </Alert>
           </>
-
-        </div>
-
-        <div className='flex w-full flex-col'>
-          <Typography variant='h4' className='mb-4'>Alternativas</Typography>
-
-
-          <div className=' flex flex-col gap-2'>
-            {['A', 'B', 'C', 'D', 'E'].map((label, index) => (
-              <>
-                <AlternativeInput
-                  key={label}
-                  label={label}
-                  onChange={(e) => handleInputChange(index, e.target.value)}
-                />
-              </>
-            ))}
+          <hr className='w-full border-gray-300'/>
+          <div className='flex h-fit w-full justify-end gap-4'>
+            <Button variant='outlined' onClick={handleBack}>Cancelar</Button>
+            <Button onClick={handleSubmit}>Salvar</Button>
           </div>
-
-          <div>
-            <h1>
-              {alternativesString}
-            </h1>
-          </div>
-
         </div>
       </div>
-      <div className='flex h-fit w-full flex-col gap-4 p-4 '>
-        <hr className='w-full border-gray-300'/>
-        <div className='flex h-fit w-full justify-end gap-4'>
-          <Button variant='outlined' size="lg" onClick={handleCancel}>Cancelar</Button>
-          <Button>Salvar</Button>
-        </div>
-      </div>
-    </div>
-
+    </>
   );
 }
 
