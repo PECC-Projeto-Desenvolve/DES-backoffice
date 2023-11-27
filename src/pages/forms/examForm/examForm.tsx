130f46f85d344ae28bec0e06f8b2ae6f78a917d4
Átila de Freitas <atila.defreitas@icloud.com>
2023-11-27 20:01:42 -0300

    exam form refactoring

--- src/pages/forms/examForm/examForm.tsx
+++ src/pages/forms/examForm/examForm.tsx
@@ -85,13 +85,12 @@ function ExamForm(): JSX.Element {
       </Dialog>
 
 
-      <div className='flex h-[100%] w-full flex-col gap-4 rounded bg-white p-4'>
-        <div className='flex h-full w-full gap-4'>
-          <div className='relative flex h-full w-full flex-col gap-2 overflow-scroll rounded-md border px-2 py-6'>
+      <div className='flex h-screen w-screen flex-col gap-4 overflow-hidden rounded bg-white px-8 py-12 transition-all'>
+        <div className='grid h-full w-full grid-cols-2 gap-2'>
+          <div className='relative flex w-full flex-col gap-2 overflow-hidden rounded-md border px-2 py-6'>
             <Typography variant='h4'>Corpo da prova</Typography>
-
-            <div
-              className='relative flex h-fit w-full flex-col gap-2 overflow-y-scroll rounded-md border p-2 transition-all'
+            <ul
+              className='grid w-full grid-cols-1 gap-4 overflow-y-scroll'
               onDrop={handleOnDrop}
               onDragOver={handleDragOver}
             >
@@ -100,20 +99,20 @@ function ExamForm(): JSX.Element {
 
                 </>
               ): (
-                <div className='flex h-full w-full select-none flex-col items-center gap-4 rounded-md bg-blue-gray-50/50 p-4'>
+                <li className='flex h-full w-full select-none flex-col items-center gap-4 rounded-md bg-blue-gray-50/50 p-4'>
                   <Typography variant='h5'>
-          clique na questão, arraste e solte aqui
+                    clique na questão, arraste e solte aqui
                   </Typography>
                   <img src={DND} className='pointer-events-none select-none'/>
-                </div>
+                </li>
               )}
               {questionOrder.map((question, index) => (
                 <>
-                  <div
+                  <li
                     key={index}
                     className='flex animate-fade-in-down select-none items-center justify-between rounded-md border bg-gray-100 p-2'
                   >
-                    {index}
+                    {index + 1}
                     <Typography variant='h6'>
                       {stringResizer(question, 50)}...
                     </Typography>
@@ -121,7 +120,7 @@ function ExamForm(): JSX.Element {
                     <IconButton onClick={() => handleRemoveQuestion(index)}>
                       <MinusCircle size={20}/>
                     </IconButton>
-                  </div>
+                  </li>
 
                 </>
               ))}
@@ -137,111 +136,36 @@ function ExamForm(): JSX.Element {
                 <>
                 </>
               )}
+
+            </ul>
+          </div>
+
+          <div className='relative flex w-full flex-col gap-2 overflow-hidden rounded-md border px-2 py-6'>
+            <Typography variant='h4' className='mb-2'>Selecione a questão</Typography>
+
+            <Input label={'Buscar'} icon={<Search size={20}/>} size='lg' />
+            <div className='relative z-30 mt-2 grid h-full max-h-full w-full grid-cols-1 flex-col gap-4 overflow-y-scroll'>
+
+              {questions.map((question, index) => (
+                <>
+                  <QuestionCard
+                    key={index}
+                    statement={question.statement}
+                    createdAt={question.createdAt}
+                    updatedAt={question.updatedAt}
+                    onDragStart={(e) => handleOnDrag(e, question.statement)}
+                  />
+                </>
+              ))}
             </div>
           </div>
 
-          <div className='flex h-full w-full flex-col gap-2 rounded-md border px-2 py-6'>
-            <div className='flex h-full w-full flex-col gap-2 rounded-md border px-2 py-6'>
-              <Typography variant='h4' className='mb-2'>Selecione a questão</Typography>
-
-              <Input label={'Buscar'} icon={<Search size={20}/>} size='lg' />
-              <div className='relative z-30 mt-2 flex h-full max-h-full flex-col gap-3'>
-
-                {questions.map((question, index) => (
-                  <>
-                    <QuestionCard
-                      key={index}
-                      statement={question.statement}
-                      createdAt={question.createdAt}
-                      updatedAt={question.updatedAt}
-                      onDragStart={(e) => handleOnDrag(e, question.statement)}
-                    />
-                  </>
-                ))}
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className='flex h-fit w-full items-end justify-end gap-4 rounded-md bg-red-300'>
-          <Button variant='outlined' onClick={handleOpen}>Cancelar</Button>
-          <Button>Salvar</Button>
-        </div>
-        {/* <div className='relative flex h-full w-full flex-col gap-2 overflow-scroll rounded-md border px-2 py-6'>
-          <Typography variant='h4'>Corpo da prova</Typography>
-
-          <div
-            className='relative flex h-fit max-h-[35rem] w-full flex-col gap-2 overflow-y-scroll rounded-md border p-2 transition-all'
-            onDrop={handleOnDrop}
-            onDragOver={handleDragOver}
-          >
-            {questionOrder.length > 0 ? (
-              <>
-
-              </>
-            ): (
-              <div className='flex h-full w-full select-none flex-col items-center gap-4 rounded-md bg-blue-gray-50/50 p-4'>
-                <Typography variant='h5'>
-                    clique na questão, arraste e solte aqui
-                </Typography>
-                <img src={DND} className='pointer-events-none select-none'/>
-              </div>
-            )}
-            {questionOrder.map((question, index) => (
-              <>
-                <div
-                  key={index}
-                  className='flex animate-fade-in-down select-none items-center justify-between rounded-md border bg-gray-100 p-2'
-                >
-                  {index}
-                  <Typography variant='h6'>
-                    {stringResizer(question, 50)}...
-                  </Typography>
-
-                  <IconButton onClick={() => handleRemoveQuestion(index)}>
-                    <MinusCircle size={20}/>
-                  </IconButton>
-                </div>
-
-              </>
-            ))}
-            {questionOrder.length > 0 ? (
-              <>
-                <Tooltip content="clique na questão, arraste e solte aqui">
-                  <div className='flex h-[4rem] w-full flex-col items-center justify-center rounded-sm bg-green-100'>
-                    <PlusCircleIcon size={24} />
-                  </div>
-                </Tooltip>
-              </>
-            ): (
-              <>
-              </>
-            )}
+          <div className='rounded-md0 col-span-2 flex h-fit w-full items-end justify-end gap-4 transition-all'>
+            <Button variant='outlined' onClick={handleOpen} size='lg'>Cancelar</Button>
+            <Button size='lg' >Salvar</Button>
           </div>
         </div>
 
-        <div className='flex h-full w-full flex-col gap-2 rounded-md border px-2 py-6'>
-          <Typography variant='h4' className='mb-2'>Selecione a questão</Typography>
-
-          <Input label={'Buscar'} icon={<Search size={20}/>} size='lg' />
-          <div className='relative z-30 mt-2 flex h-full max-h-full flex-col gap-3'>
-
-            {questions.map((question, index) => (
-              <>
-                <QuestionCard
-                  key={index}
-                  statement={question.statement}
-                  createdAt={question.createdAt}
-                  updatedAt={question.updatedAt}
-                  onDragStart={(e) => handleOnDrag(e, question.statement)}
-                />
-              </>
-            ))}
-          </div>
-        </div>
-        <div className='col-span-2 -mb-16 flex h-[3rem] w-full items-end justify-end gap-4 rounded-md'>
-          <Button variant='outlined' onClick={handleOpen}>Cancelar</Button>
-          <Button>Salvar</Button>
-        </div> */}
       </div>
     </>
   );
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import { Home } from '../pages/home/home';
 import { Button, Navbar, Typography } from '@material-tailwind/react';
 import { LogOut } from 'lucide-react';
@@ -11,6 +11,9 @@ import { QuestionForm } from '../pages/forms/questionForm/questionForm';
 import { ExamForm } from '../pages/forms/examForm/examForm';
 
 function AppRoutes(): JSX.Element {
+  const [isForm, setIsForm] = React.useState<boolean>(false);
+  const location = useLocation();
+  const paths = location.pathname;
 
   const navigate = useNavigate();
 
@@ -19,25 +22,38 @@ function AppRoutes(): JSX.Element {
   };
 
   return (
-    <div className="relative flex h-screen w-screen flex-col gap-4 bg-blue-gray-50 p-4 px-2 transition-all sm:px-2 md:px-8 lg:px-32">
-      <Navbar fullWidth className='mx-auto flex h-fit items-center justify-between rounded-md p-4 '>
-        <Typography variant="h5" color="black" onClick={handleNavigate} className='cursor-pointer transition-all hover:text-2xl'>
-          Desenvolve Backoffice
-        </Typography>
+    <>
+      { paths.includes('exam/form') ? (
+        <>
+          <Routes>
+            <Route path='/question/form' element={<QuestionForm />}/>
+            <Route path='/exam/form' element={<ExamForm />}/>
+          </Routes>
+        </>
+      ) : (
+        <div className="relative flex h-screen w-screen flex-col gap-4 bg-blue-gray-50 p-4 px-2 transition-all sm:px-2 md:px-8 lg:px-32">
+          <Navbar fullWidth className='mx-auto flex h-fit items-center justify-between rounded-md p-4'>
+            <Typography variant="h5" color="black" onClick={handleNavigate} className='cursor-pointer transition-all hover:text-2xl'>
+          Desenvolve Backoffice {paths}
+            </Typography>
 
-        <Button className="flex items-center gap-4">
+            <Button className="flex items-center gap-4">
           Sair <LogOut size={20} />
-        </Button>
-      </Navbar>
-      <Routes>
-        <Route path='/' element={<Home />}/>
+            </Button>
+          </Navbar>
 
-        <Route path='/question/form' element={<QuestionForm />}/>
-        <Route path='/exam/form' element={<ExamForm />}/>
 
-        <Route path='/exam' element={<Exam />}/>
-      </Routes>
-    </div>
+          <Routes>
+            <Route path='/' element={<Home />}/>
+
+            <Route path='/question/form' element={<QuestionForm />}/>
+            {/* <Route path='/exam/form' element={<ExamForm />}/> */}
+
+            <Route path='/exam' element={<Exam />}/>
+          </Routes>
+        </div>
+      )}
+    </>
   );
 }
 
