4f878b47ee4e09ff065bb294d51dc995a51ccf97
Átila de Freitas <atila.defreitas@icloud.com>
2024-01-14 10:15:29 -0300

    setting up external components for exam and simulation selection

--- src/pages/process/Process.tsx
+++ src/pages/process/Process.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { IconButton, Option, Select, Typography } from '@material-tailwind/react';
+import { Typography } from '@material-tailwind/react';
 import { BackButton } from '../../components/BackButton';
 // import { formatDate } from '../../utils';
-import { Eye, Trash } from 'lucide-react';
+import { ExamSelection } from './components/ExamSelection';
+import { SimulationSelection } from './components/SimulationSelection';
 
 function Process() {
   const [exams, setExams] = React.useState([]);
   const [selectedExamID, setSelectedExamID] = React.useState('');
+
   const [selectedExam, setSelectedExam] = React.useState({
     title: '',
     id: '',
@@ -25,12 +27,10 @@ function Process() {
   };
 
   const fetchExams = () => {
-
     fetch(`${import.meta.env.VITE_API_URL}/exams`)
       .then(response => response.json())
       .then(data => {
         setExams(data);
-
       })
       .catch(error => {
         console.error('Erro ao buscar provas:', error);
@@ -51,125 +51,30 @@ function Process() {
         </div>
         <div className='flex h-full w-full flex-col'>
           <span className='mb-2'>
-            <Typography variant='h4' className='dark:text-white'>Cadastrar novo processo</Typography>
+            <Typography variant='h4' className='dark:text-white'>Configurar processo</Typography>
           </span>
 
-          <div className='mb-8'>
-            <Typography variant='lead' className='dark:text-white'>Atribua uma prova para um processo</Typography>
-            <>
-              {selectedExamID ?
-                (<>
-                  <div className='flex w-full items-center justify-between rounded-lg border bg-white p-4'>
-                    <span>
-                      <Typography variant='h5'>{selectedExam.title}</Typography>
-                      <Typography variant='small'>{selectedExam.id}</Typography>
-                    </span>
-
-                    <span className='flex gap-2'>
-                      <IconButton color='orange' disabled>
-                        <Eye />
-                      </IconButton>
-                      <IconButton color='red' onClick={() => {
-                        setSelectedExam({
-                          title: '',
-                          id: '',
-                        });
-                        setSelectedExamID('');
-                      }}>
-                        <Trash />
-                      </IconButton>
-                    </span>
-                  </div>
-                </>)
-                :
-                <Select
-                  label="Provas"
-                  size='lg'
-                  onFocus={handleFocus}
-                  labelProps={{ className: 'dark:text-white text-black' }}
-                  className='bg-white/80 dark:bg-blue-gray-200/20'
-                  onChange={(value) => {
-                    setSelectedExamID(value);
-
-                    fetch(`${import.meta.env.VITE_API_URL}/exams/${value}`)
-                      .then(response => response.json())
-                      .then(data => {
-                        setSelectedExam(data);
-                      })
-                      .catch(error => {
-                        console.error('Erro ao buscar provas:', error);
-                      // setIsLoading(false);
-                      });
-                  }}
-                >
-                  {exams.map((exam, index) => (
-                    <Option value={exam.id} index={index} key={index} className='mb-2 border'>
-                      <Typography variant='h6'>{exam.title}</Typography>
-                    </Option>
-                  ))}
-                </Select>
-              }
-            </>
-          </div>
-
           <div>
-            <Typography variant='lead' className='dark:text-white'>Atribua um simulado</Typography>
-            <Typography variant='small' className='dark:text-white'>O simulado serve para o candidato praticar</Typography>
-            <>
-              {selectedSimulationID ?
-                (<>
-                  <div className='flex w-full items-center justify-between rounded-lg border bg-white p-4'>
-                    <span>
-                      <Typography variant='h5'>{selectedSimulation.title}</Typography>
-                      <Typography variant='small'>{selectedSimulation.id}</Typography>
-                    </span>
 
-                    <span className='flex gap-2'>
-                      <IconButton color='orange' disabled>
-                        <Eye />
-                      </IconButton>
-                      <IconButton color='red' onClick={() => {
-                        setSelectedSimulation({
-                          title: '',
-                          id: '',
-                        });
-                        setSelectedSimulationID('');
-                      }}>
-                        <Trash />
-                      </IconButton>
-                    </span>
-                  </div>
-                </>)
-                :
-                <Select
-                  label="Provas"
-                  size='lg'
-                  onFocus={handleFocus}
-                  labelProps={{ className: 'dark:text-white text-black' }}
-                  className='bg-white/80 dark:bg-blue-gray-200/20'
-                  onChange={(value) => {
-
-                    setSelectedSimulationID(value);
-
-                    fetch(`${import.meta.env.VITE_API_URL}/exams/${value}`)
-                      .then(response => response.json())
-                      .then(data => {
-                        setSelectedSimulation(data);
-                      })
-                      .catch(error => {
-                        console.error('Erro ao buscar provas:', error);
-                      });
-                  }}
-                >
-                  {exams.map((exam, index) => (
-                    <Option value={exam.id} index={index} key={index} className='mb-2 border'>
-                      <Typography variant='h6'>{exam.title}</Typography>
-                    </Option>
-                  ))}
-                </Select>
-              }
-            </>
           </div>
+
+          <ExamSelection
+            exams={exams}
+            selectedExam={selectedExam}
+            selectedExamID={selectedExamID}
+            setSelectedExam={setSelectedExam}
+            setSelectedExamID={setSelectedExamID}
+            handleFocus={handleFocus}
+          />
+
+          <SimulationSelection
+            exams={exams}
+            selectedSimulation={selectedSimulation}
+            selectedSimulationID={selectedSimulationID}
+            setSelectedSimulation={setSelectedSimulation}
+            setSelectedSimulationID={setSelectedSimulationID}
+            handleFocus={handleFocus}
+          />
         </div>
       </section>
     </>
